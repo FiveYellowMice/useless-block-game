7ca3b74faf7525a0771acdceb7b278b5f4f7dd76
Kaizhe Huang <hkz85825915@gmail.com>
2022-03-03 02:42:02 +1100

    Enable GL debug callback

--- main.cpp
+++ main.cpp
@@ -44,6 +44,27 @@ int main(int argc, char* argv[]) {
       throw ApplicationException("Necessary OpenGL extensions not supported");
     }
 
+    if (GLEW_KHR_debug) {
+      glEnable(GL_DEBUG_OUTPUT);
+      glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+      glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, false); // disable notifications
+      glDebugMessageCallback([] (GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+        switch (severity) {
+          case GL_DEBUG_SEVERITY_HIGH:
+            std::cerr << "GL Error: ";
+            break;
+          case GL_DEBUG_SEVERITY_MEDIUM:
+            std::cerr << "GL Warning: ";
+            break;
+          default:
+            std::cerr << "GL Message: ";
+        }
+        std::cerr << "(" << id << ") " << message << std::endl;
+      }, nullptr);
+    } else {
+      std::cerr << "Debug output unavailable" << std::endl;
+    }
+
     glfwSetFramebufferSizeCallback(window, [] (GLFWwindow* window, int width, int height) {
       glViewport(0, 0, width, height);
     });
